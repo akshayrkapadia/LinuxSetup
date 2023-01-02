205b65a0fe553e82cf39ddbdf4d34f84a69d16af
Akshay R. Kapadia <akshayrajankapadia@gmail.com>
2023-01-02 16:28:26 -0500

    Updated

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 sudo apt install tmux zsh vim python3-pip curl # Install software 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended # Install Oh-My-Zsh package manager
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # Install Oh-My-Zsh package manager
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions # Zsh autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting # Zsh syntax highlighting
 sudo chsh -s $(which zsh) # Make zsh default shell
